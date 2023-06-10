a065d697f3f601761c13c508cd9e84ccb557e9b5
ozkanonur <work@onurozkan.dev>
2023-06-10 21:27:10 +0300

    add dependencies in index db
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- lpm-builder/pkg/builder/index_patch.go
+++ lpm-builder/pkg/builder/index_patch.go
@@ -17,9 +17,21 @@ func GenerateIndexPatch(ctx *BuilderCtx) {
 
 	timestamp := time.Now().UTC().Unix()
 
-	insertPart := "INSERT INTO repository (name, description, maintainer, source_repository, kind, tags, installed_size, license, v_major, v_minor, v_patch, v_tag, v_readable, index_timestamp)"
+	insertPart := "INSERT INTO repository (name, description, maintainer, source_repository, kind, tags, installed_size, license, v_major, v_minor, v_patch, v_tag, v_readable, mandatory_dependencies, suggested_dependencies, index_timestamp)"
 
-	valuesPart := fmt.Sprintf(`VALUES ("%s", "%s", "%s", "%s", "%s", "%s", %d, "%s", %d, %d, %d, "%s", "%s", %d);`,
+	var mandatoryDependencies []string
+	for _, val := range ctx.TemplateFields.MandatoryDependencies.Runtime {
+		pkg_with_version := fmt.Sprintf("%s@%s%s", val.Name, val.Version.Condition, val.Version.ReadableFormat)
+		mandatoryDependencies = append(mandatoryDependencies, pkg_with_version)
+	}
+
+	var suggestedDependencies []string
+	for _, val := range ctx.TemplateFields.SuggestedDependencies.Runtime {
+		pkg_with_version := fmt.Sprintf("%s@%s%s", val.Name, val.Version.Condition, val.Version.ReadableFormat)
+		suggestedDependencies = append(suggestedDependencies, pkg_with_version)
+	}
+
+	valuesPart := fmt.Sprintf(`VALUES ("%s", "%s", "%s", "%s", "%s", "%s", %d, "%s", %d, %d, %d, "%s", "%s", "%s", "%s", %d);`,
 		ctx.TemplateFields.Name,
 		ctx.TemplateFields.Description,
 		ctx.TemplateFields.Maintainer,
@@ -33,6 +45,8 @@ func GenerateIndexPatch(ctx *BuilderCtx) {
 		ctx.TemplateFields.Version.Patch,
 		tag,
 		ctx.TemplateFields.Version.ReadableFormat,
+		strings.Join(mandatoryDependencies, ","),
+		strings.Join(suggestedDependencies, ","),
 		timestamp,
 	)
 
